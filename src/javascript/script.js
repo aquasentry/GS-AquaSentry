c4da600b9f8865549b6a07df9c675c2c04885740
Ianny Raquel <iannyraquel252488@gmail.com>
2024-06-07 08:47:45 -0300

    Mudando o background-image

---
+++ src/javascript/script.js
@@ -0,0 +1,14 @@
+// const images = ['./src/imagens/slideshow/imagem2.jpg', './src/imagens/slideshow/imagem3.jpg', './src/imagens/slideshow/imagem4.png'];
+// let currentIndex = 0;
+
+// function changeSlide() {
+//     const imageElement = document.getElementById('imagem-background');
+//     imageElement.src = images[currentIndex];
+//     currentIndex = (currentIndex + 1) % images.length;
+// }
+
+// // Chama a função inicialmente para definir a primeira imagem do slideshow
+// changeSlide();
+
+// // Define um intervalo para chamar a função de mudar o slide a cada 5 segundos (5000 milissegundos)
+// setInterval(changeSlide, 1000);
